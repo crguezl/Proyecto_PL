3b9d5fb99582162261ed329c6d369b8a9e8c0622
CrmPrograming <crmprograming@gmail.com>
2013-05-14 22:10:12 +0100

    Añadido botón para descargar el fichero generado correspondiente al examen

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,23 @@
+var result = "";
+
 $(document).ready(function(){
-  $("#B_GENERAR").click(function(){    
+  $("#B_GENERAR").click(function(){   
+    document.getElementById('OUTPUT').className = 'unhidden'; 
+    document.getElementById('B_DESCARGAR').className = 'unhidden';
      try {
-       var result = generador_examen.parse($("#TA_INPUT").val());
+       result = generador_examen.parse($("#TA_INPUT").val());
        $("#OUTPUT").html(result);
      } catch (e) {
        $("#OUTPUT").html(String(e));
      }	  
   });
+  $("#B_DESCARGAR").click(function(){
+    download();
+  });
 });
+
+function download() {
+ 
+  location.href='data:application/download,' + encodeURIComponent(result);
+  
+}
